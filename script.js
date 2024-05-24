9defb145353a89788166df841406957c5967189c
devid astil <111349164+DevidAstil@users.noreply.github.com>
2024-05-24 15:56:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.services_items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+        variableWidth: true
+      });
+  });
